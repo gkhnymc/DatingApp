91815cfd0deece07cbf46ee2789ccda70e833a5f
Gokhan <gkhnymc1@gmail.com>
2023-05-11 12:43:07 +0200

    End of section 5

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -23,7 +23,7 @@ namespace API.Controllers
         [HttpPost("register")] // POST: api/account/register
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if (await UserExists(registerDto.Username))
+            if (await UserExists(registerDto.Username)) 
             {
                 return BadRequest("Username is taken");
             }
